1eeb08108506960ccb7a0becf595405ea13252ab
fluctuacia <fluctuacia@gmail.com>
2022-04-19 19:19:50 -0700

    Exercise 1: update font

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -5,7 +5,21 @@ const PhotoGridItem = ({ id, src, alt, tags }) => {
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+        <picture>
+          <source type="image/avif"
+                  srcSet={`
+                  ${src.replace('.jpg', '.avif')} 1x,
+                  ${src.replace('.jpg', '@2x.avif')} 2x,
+                  ${src.replace('.jpg', '@3x.avif')} 3x
+                  `} />
+          <source type="image/jpeg"
+                  srcSet={`
+                  ${src} 1x,
+                  ${src.replace('.jpg', '@2x.jpeg')} 2x,
+                  ${src.replace('.jpg', '@3x.jpeg')} 3x
+                  `} />
+          <Image src={src} alt={alt} />
+        </picture>
       </Anchor>
       <Tags>
         {tags.map((tag) => (
@@ -28,6 +42,7 @@ const Image = styled.img`
   height: 300px;
   border-radius: 2px;
   margin-bottom: 8px;
+  object-fit: cover;
 `;
 
 const Tags = styled.ul`
